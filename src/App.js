c92899e27bbefbc30b434241c52e5e5cc0214a70
my-portfolio <beksultan1709@gmail.com>
2024-07-13 23:44:32 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+      <Main/>
+      <Footer/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+	return (
+		<div>
+			<h1 className='text'>Путь Javascript</h1>
+			header
+		</div>
+	);
+};
+
+export default Header;
