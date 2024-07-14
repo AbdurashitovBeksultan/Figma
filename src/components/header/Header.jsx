8be9e28dc94effec9ea0327eee45e4533c5df12b
my-portfolio <beksultan1709@gmail.com>
2024-07-14 19:59:50 +0600

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
+import path from "../../assets/png/Path 3.png";
+import pach from "../../assets/png/Path 4.png";
+import Placeholder from "react-placeholder";
 
 const Header = () => {
 	return (
-		<div>
-			<h1 className='text'>Путь Javascript</h1>
-			header
-      bekafront 
-      <h1>frontend developer</h1>
-		</div>
+		<>
+			<header className="header">Welcome to JavaScript</header>
+			<div className="component_div">
+				<Placeholder
+					type="media"
+					mediaStyle={{ width: 100, height: 100 }} 
+					ready={!path}
+				>
+					{/* <img src={path} alt="HTML" className="img_foto" /> */}
+				</Placeholder>
+				<div className="img">
+					<img src={path} alt="HTML" className="img_foto" />
+					<h1 className="text">Путь Javascript</h1>
+					<img src={pach} alt="CSS" className="img_foto" />
+				</div>
+			</div>
+		</>
 	);
 };
 
